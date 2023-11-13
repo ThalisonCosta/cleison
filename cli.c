fe74d97586562981c9ece8a51daae7890b46836c
ThalisonCosta <thalisoncosta123@gmail.com>
2023-11-12 21:48:13 -0400

    :construction: append strings

--- cli.c
+++ cli.c
@@ -9,25 +9,51 @@ int main() {
     scanf("%d", &action);
 
     if (action == 1) {
-      char key[100]; 
-      char value[100];
-      char *h_key;
-      char *h_value;
+      char continue_inside[5];
+      char rows[4098];
 
-      printf("Insira Nome do Campo: ");
-      scanf("%s", key);
-      h_key = (char *)malloc((strlen(key) + 1) * sizeof(char));
-      strcpy(h_key, key);
+      while (1) {
+        char key[100];
+        char value[100];
+        char *h_key;
+        char *h_value;
+        char row[512];
 
-      printf("Insira Valor do Campo: ");
-      scanf("%s", value);
-      h_value = (char *)malloc((strlen(value) + 1) * sizeof(char));
-      strcpy(h_value, value);
+        printf("Insira Nome do Campo: ");
+        scanf("%s", key);
+        h_key = (char *)malloc((strlen(key) + 1) * sizeof(char));
+        strcpy(h_key, key);
 
-      printf("\n{\n\t\"%s\":\"%s\"\n}\n", h_key, h_value);
+        printf("Insira Valor do Campo: ");
+        scanf("%s", value);
+        h_value = (char *)malloc((strlen(value) + 1) * sizeof(char));
+        strcpy(h_value, value);
 
-      free(h_key); 
-      free(h_value);
+        sprintf(row, "\n\t\"%s\":\"%s\",\n", h_key, h_value);
+
+        strcat(rows, row);
+        printf("\n{%s}\n", rows);
+
+        char stop[] = "nao";
+        printf("Quer continuar? ");
+        scanf("%s", continue_inside);
+
+        int result;
+        result = strcmp(stop, continue_inside);
+
+        if (result == 0) {
+          char filename[300];
+          printf("Qual o nome do arquivo? ");
+          scanf("%s", filename);
+
+          free(h_key);
+          free(h_value);
+
+          // TODO: save into a file
+          printf("saved your new json to %s\n", filename);
+          break;
+        }
+      }
     } else {
       printf("Visualizar JSONs\n");
     }
