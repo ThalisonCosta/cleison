72d8e1566afa93edb58adb412b2b20cbc320e731
ThalisonCosta <thalisoncosta123@gmail.com>
2023-11-12 21:04:04 -0400

    :construction: store values in heap

--- cli.c
+++ cli.c
@@ -1,30 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main() {
-  int action;
-
   while (1) {
     printf("1 - Criar JSON\n2 - Visualizar JSONs\n");
-    action = scanf("%d", &action);
+    int action;
+    scanf("%d", &action);
 
     if (action == 1) {
-      char key;
-      char value;
+      char key[100]; 
+      char value[100];
+      char *h_key;
+      char *h_value;
 
       printf("Insira Nome do Campo: ");
-      scanf("%s", &key);
+      scanf("%s", key);
+      h_key = (char *)malloc((strlen(key) + 1) * sizeof(char));
+      strcpy(h_key, key);
 
       printf("Insira Valor do Campo: ");
-      scanf("%s", &value);
+      scanf("%s", value);
+      h_value = (char *)malloc((strlen(value) + 1) * sizeof(char));
+      strcpy(h_value, value);
 
-      printf("\n%s:%s\n", &key, &value);
+      printf("\n{\n\t\"%s\":\"%s\"\n}\n", h_key, h_value);
 
+      free(h_key); 
+      free(h_value);
     } else {
-      printf("visualizar jsons\n");
+      printf("Visualizar JSONs\n");
     }
 
     break;
   }
-  return 1;
+
+  return 0;
 }
